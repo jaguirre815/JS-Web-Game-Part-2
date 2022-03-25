c01bb080e50623f767435fcc03530eb714c3151f
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-03-24 20:30:36 -0500

    done

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        addToInventory(url);
+    })
 }
 
 function newInventory(){
@@ -26,9 +30,17 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory;
 }
 
-newInventory()
+function addToInventory (imgURL) {
+    let inventoryItem = document.createElement('img')
+    inventoryItem.src = imgURL
+    inventory.append(inventoryItem)
+}
+
+const inventory = newInventory()
+
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
